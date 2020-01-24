2c2825dd730a834970f67df1dec2cfbc45fcdfea
Sudip Adhikari <sudip.adhikari01@gmail.com>
2020-01-24 16:39:00 +0545

    register form validation done

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
+import Joi from "joi-browser";
 
 class Register extends Component {
   state = {
     register: { username: "", email: "", password: "", confirmPassword: "" },
     errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    email: Joi.string()
+      .email()
+      .max(256)
+      .required()
+      .label("Email"),
+    password: Joi.string()
+      .min(8)
+      .required()
+      .label("Password"),
+    confirmPassword: Joi.string()
+      .min(8)
+      .required()
+      .label("Confirm Password")
+      .valid(Joi.ref("password"))
   };
 
   registerStyle = {
@@ -17,13 +38,50 @@ class Register extends Component {
   handleSubmit = event => {
     event.preventDefault();
     console.log("form submitted");
+    const errors = this.joiValidate();
+    if (errors) {
+      this.setState({ errors });
+    }
   };
 
   handleChange = ({ currentTarget: input }) => {
     const register = { ...this.state.register };
     register[input.name] = input.value;
     this.setState({ register });
-    console.log("input element changed");
+    // validateInput(input);
+    const errors = {};
+    const error = this.validateInputProperty(input);
+
+    if (error) {
+      errors[error.details[0].path[0]] = error.details[0].message;
+    }
+    this.setState({ errors });
+  };
+
+  // validate input property
+
+  validateInputProperty = input => {
+    const obj = { [input.name]: input.value };
+    const schema = { [input.name]: this.schema[input.name] };
+
+    const result = Joi.validate(obj, schema);
+    if (!result.error) return null;
+
+    return result.error;
+  };
+
+  // validate
+  joiValidate = () => {
+    const result = Joi.validate(this.state.register, this.schema, {
+      abortEarly: false
+    });
+    if (!result.error) return null;
+    const errors = {};
+
+    for (let item of result.error.details) {
+      errors[item.path[0]] = item.message;
+    }
+    return errors;
   };
 
   render() {
@@ -40,6 +98,11 @@ class Register extends Component {
                 name="username"
                 onChange={this.handleChange}
               />
+              {this.state.errors.username && (
+                <Form.Text className="alert alert-danger">
+                  {this.state.errors.username}
+                </Form.Text>
+              )}
             </Form.Group>
 
             <Form.Group controlId="email">
@@ -50,6 +113,12 @@ class Register extends Component {
                 name="email"
                 onChange={this.handleChange}
               />
+
+              {this.state.errors.email && (
+                <Form.Text className="alert alert-danger">
+                  {this.state.errors.email}
+                </Form.Text>
+              )}
             </Form.Group>
 
             <Form.Group controlId="password">
@@ -60,6 +129,12 @@ class Register extends Component {
                 name="password"
                 onChange={this.handleChange}
               />
+
+              {this.state.errors.password && (
+                <Form.Text className="alert alert-danger">
+                  {this.state.errors.password}
+                </Form.Text>
+              )}
             </Form.Group>
 
             <Form.Group controlId="confirmPassword">
@@ -70,6 +145,11 @@ class Register extends Component {
                 name="confirmPassword"
                 onChange={this.handleChange}
               />
+              {this.state.errors.confirmPassword && (
+                <Form.Text className="alert alert-danger">
+                  {this.state.errors.confirmPassword}
+                </Form.Text>
+              )}
             </Form.Group>
             <Form.Group controlId="formCheckBox">
               <Form.Check type="checkbox" label="Check me out" />
